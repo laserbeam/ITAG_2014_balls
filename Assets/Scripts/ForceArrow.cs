2b8b85d712af6f3792dc9b6f05c18057026cf696
Catalin Balan <laserbeam333@gmail.com>
2014-10-11 14:26:15 +0200

    force arrows working properly

--- Assets/Scripts/ForceArrow.cs
+++ Assets/Scripts/ForceArrow.cs
@@ -3,9 +3,44 @@ using System.Collections;
 
 public class ForceArrow : MonoBehaviour {
 
+	private LineRenderer lineRenderer;
+
+	public Color attractColor;
+	public Color repelColor;
+
+	private Vector2 forceStorage;
+	public Vector2 force {
+		get {
+			return forceStorage;
+		}
+		set {
+			Debug.Log ("CALLED " + value);
+			forceStorage = value;
+			lineRenderer.SetPosition ( 1, new Vector3 ( value.x, value.y ));
+		}
+	}
+	public enum ForceKind {
+		REPEL,
+		ATTRACT
+	};
+	private ForceKind kindStorage;
+	public ForceKind kind {
+		get {
+			return kindStorage;
+		}
+		set {
+			kindStorage = value;
+			if (value == ForceKind.ATTRACT) {
+				GetComponent<LineRenderer>().SetColors ( attractColor, Color.white );
+			} else if (value == ForceKind.REPEL) {
+				GetComponent<LineRenderer>().SetColors ( repelColor, Color.white );
+			}
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
-	
+		lineRenderer = GetComponent<LineRenderer>();
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/Simulation.cs
+++ Assets/Scripts/Simulation.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Simulation : MonoBehaviour {
+
+	public GameObject forceArrowPrefab;
 
 	private bool isRunningStorage = false;
 	public bool isRunning {
@@ -21,6 +24,7 @@ public class Simulation : MonoBehaviour {
 	private GameObject[] attractors;
 	private GameObject[] repellers;
 	private GameObject player;
+	private Dictionary<int, GameObject> forceArrows;
 
 	// Use this for initialization
 	void Start () {
@@ -32,7 +36,25 @@ public class Simulation : MonoBehaviour {
 		attractors = GameObject.FindGameObjectsWithTag("Attractor");
 		repellers = GameObject.FindGameObjectsWithTag("Repeller");
 		player = GameObject.FindGameObjectWithTag("Player");
+		forceArrows = new Dictionary<int, GameObject>();
 		isRunning = true;
+
+		foreach (GameObject attractor in attractors) {
+			GameObject forceArrow = (GameObject) GameObject.Instantiate ( forceArrowPrefab, Vector3.zero, Quaternion.identity );
+			forceArrow.transform.parent = player.transform;
+			forceArrow.transform.position = player.transform.position;
+			forceArrows[attractor.GetInstanceID()] = forceArrow;
+			forceArrow.GetComponent<ForceArrow>().kind = ForceArrow.ForceKind.ATTRACT;
+		}
+
+		foreach (GameObject repeller in repellers) {
+			GameObject forceArrow = (GameObject) GameObject.Instantiate ( forceArrowPrefab, Vector3.zero, Quaternion.identity );
+			forceArrow.transform.parent = player.transform;
+			forceArrow.transform.position = player.transform.position;
+			forceArrows[repeller.GetInstanceID()] = forceArrow;
+			forceArrow.GetComponent<ForceArrow>().kind = ForceArrow.ForceKind.REPEL;
+		}
+
 	}
 
 	// Update is called once per frame
@@ -42,15 +64,17 @@ public class Simulation : MonoBehaviour {
 			foreach (GameObject attractor in attractors) {
 				Vector2 dir = (attractor.transform.position - player.transform.position).normalized;
 				body.AddForce( dir, ForceMode2D.Force );
+				ForceArrow arrow = forceArrows[attractor.GetInstanceID()].GetComponent<ForceArrow>();
+				arrow.force = dir;
 				Vector3 dir3 = new Vector3 ( dir.x, dir.y );
-				Debug.DrawLine ( player.transform.position, player.transform.position + dir3, Color.blue );
 			}
 
 			foreach (GameObject repeller in repellers) {
 				Vector2 dir = -(repeller.transform.position - player.transform.position).normalized;
 				body.AddForce( dir, ForceMode2D.Force );
+				ForceArrow arrow = forceArrows[repeller.GetInstanceID()].GetComponent<ForceArrow>();
+				arrow.force = dir;
 				Vector3 dir3 = new Vector3 ( dir.x, dir.y );
-				Debug.DrawLine ( player.transform.position, player.transform.position + dir3, Color.red );
 			}
 		}
 	}
