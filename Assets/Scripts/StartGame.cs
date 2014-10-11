a43974846aa8eae227e83cb13f196660fc0666f6
Catalin Balan <laserbeam333@gmail.com>
2014-10-11 16:43:21 +0200

    play button

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void StartTheGame () {
+		Application.LoadLevel ( "Play" );
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
