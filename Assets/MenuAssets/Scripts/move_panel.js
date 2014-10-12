177110f2d019e918d079f2da5b8a23e48ec09e25
CostinGeorge <costin.mihaita10@gmail.com>
2014-10-12 13:53:24 +0200

    Menu 3

---
+++ Assets/MenuAssets/Scripts/move_panel.js
@@ -0,0 +1,24 @@
+﻿
+public function PlayButton () {
+	this.transform.position.y = -50 ;
+}
+   
+public function PlaySettings () {
+	this.animation.Play("Move_planesAnim");
+}   
+   
+public function PlayCredits () {
+	this.animation.Play("Credits");
+}
+
+public function QuitMenuPosition () {
+	this.transform.position= new Vector3(0,0,20) ;
+}   
+
+public function QuitMenuPosition2 () {
+	this.transform.position= new Vector3(0,-50,20) ;
+}
+
+public function QuitPanelPos() {
+	this.transform.position= new Vector3(0,-50,20) ;
+}
